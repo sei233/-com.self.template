7cddb2c0ab6661887e82f50eef84c51dd6836360
石乾 <1136420833@qq.com>
2017-11-09 16:07:39 +0800

    底部导航栏

--- app/src/main/java/com/self/template/history/record.java
+++ app/src/main/java/com/self/template/history/record.java
@@ -5,4 +5,27 @@ package com.self.template.history;
  */
 
 public class record {
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public record(String name, String location, String state) {
+        this.name = name;
+        this.location = location;
+        this.state = state;
+    }
+
+    private String name;
+    private String location;
+    private String state;
+
 }
